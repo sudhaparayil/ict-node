73178458bd9bf4ae61f056627017e3c3711f3915
sudhaparayil <33888505+sudhaparayil@users.noreply.github.com>
2018-10-28 20:51:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 console.log('My nodejs');
 https://github.com/BrentAureli/Youtube-Tutorials/tree/master/NodeJS%20Tutorials
 https://grokonez.com/frontend/angular/angular-6/angular-6-upload-files-download-files-to-node-js-restapis-server-express-multer-bootstrap
+
+
+mongodb://<dbuser>:<dbpassword>@ds139954.mlab.com:39954/meandb
+
+https://mlab.com/databases/meandb
+
+sudha
+sudha123
