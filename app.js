67debab61d8a0ce699cf244d741fa07c58ef9a58
sudhaparayil <33888505+sudhaparayil@users.noreply.github.com>
2018-11-05 18:11:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,10 @@ angular6 %% employee payroll
 https://www.youtube.com/watch?v=sWOXETusJNw&index=18&list=WL
 
 https://github.com/AmitXShukla/Employee-Payroll-Salary-App-Angular-6-MEAN-Stack
+
+
+crud operation angular7
+
+https://www.youtube.com/watch?v=jYvkMv7LzCw
+
+https://github.com/CodAffection/Angular-7-CRUD-with-Web-API
